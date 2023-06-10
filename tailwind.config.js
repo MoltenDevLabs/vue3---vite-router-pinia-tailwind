eee715580426305f08d61cc60999dafad58bc2a3
Pol Cadavid <polkadavid@gmail.com>
2023-06-10 12:38:08 +0200

    added default typography styles and contact view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {},
-  },
-  fontFamily: {
-    'header': ['Lato', 'sans-serif'], //Header, subtitle 
-    'body': ['Merriweather', 'sans-serif'] //Body, button, caption, overline
+    fontFamily: {
+      header: ["Lato", "sans-serif"], //Header, subtitle
+      body: ["Merriweather", "sans-serif"], //Body, button, caption, overline
+    },
   },
   plugins: [],
 };
